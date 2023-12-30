240d949d992bbac98a5bc8f3d3e8ca105c7fb3fc
lkx <lkx3091705416@126.com>
2023-12-30 20:06:07 +0800

    对前端项目进行优化,以及编写登录页面以及逻辑

---
+++ src/router/routes.ts
@@ -0,0 +1,52 @@
+import { RouteRecordRaw } from "vue-router";
+import UserLayout from "@/layouts/UserLayout.vue";
+import UserLoginView from "@/views/user/UserLoginView.vue";
+import UserRegisterView from "@/views/user/UserRegisterView.vue";
+import NoAuthView from "@/views/NoAuthView.vue";
+import ACCESS_ENUM from "@/access/accessEnum";
+import AdminView from "@/views/AdminView.vue";
+
+export const routes: Array<RouteRecordRaw> = [
+  {
+    path: "/user",
+    name: "用户",
+    component: UserLayout,
+    children: [
+      {
+        path: "/user/login",
+        name: "用户登录",
+        component: UserLoginView,
+      },
+      {
+        path: "/user/register",
+        name: "用户注册",
+        component: UserRegisterView,
+      },
+    ],
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/noAuth",
+    name: "无权限",
+    component: NoAuthView,
+    meta: {
+      hideInMenu: true,
+    },
+  },
+  {
+    path: "/admin",
+    name: "管理员可见",
+    component: AdminView,
+    meta: {
+      access: ACCESS_ENUM.ADMIN,
+    },
+  },
+  {
+    path: "/about",
+    name: "关于我的",
+    component: () =>
+      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
+  },
+];
---
+++ src/views/AdminView.vue
@@ -0,0 +1,8 @@
+<template>
+  <div class="home">
+    管理员可见
+    <img alt="Vue logo" src="../assets/logo.jpg" />
+  </div>
+</template>
+
+<script lang="ts" setup></script>
